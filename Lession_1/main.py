9c4dfc203c4b9744c70cd3fee932ba54c82bec2f
Anh Hoang <anh.hoang@gmail.com>
2025-04-12 06:51:49 +0300

    Learn Python

---
+++ Lession_1/main.py
@@ -0,0 +1,21 @@
+# This is my first Python file
+print("Hello World of Pizza!")
+print("It is really good")
+
+# Variables - string,integer, float, boolean
+#String
+first_name = "Anh"
+email = "Broidi@gmail.com"
+
+#Integer
+age = 30
+#Float
+score = 10.5
+# Boolean
+is_student = True
+print(first_name)
+print(f"Hello {first_name}")
+print(f"Your email is {email}")
+print(f"Your age is {age}")
+print(f"Your score is {score}")
+print(f"Are you a student? {is_student}")
