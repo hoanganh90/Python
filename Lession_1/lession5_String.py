2449cebacd111aa0722d6ba8164a70bf0881db19
Anh Hoang <anh.comdr@gmail.com>
2025-04-19 07:18:23 +0300

    String

---
+++ Lession_1/lession5_String.py
@@ -0,0 +1,13 @@
+# string concatenation
+print('hello' + ' world')
+print('hello' + '5')
+#print('hello' + 5 ) # TypeError: can only concatenate str (not "int") to str
+print(str(199))
+# Esacape sequence
+weather  = 'It\'s sunny'
+print(weather)
+weather2  = 'It\'s \"kind of\" sunny'
+print(weather2)
+
+weather3  = '\t It\'s \"kind of\" \n sunny'
+print(weather3)
