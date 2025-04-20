1b1c6a187681167cf549c2a9296c01461ee8ec20
Anh Hoang <anh.comdr@gmail.com>
2025-04-20 07:44:10 +0300

    Sort and reverse

--- Fundamental 2/Lession3_List_Method.py
+++ Fundamental 2/Lession3_List_Method.py
@@ -13,5 +13,14 @@ print(basket)
 # Removing from a list - REMOVE
 basket.remove(10)  # Remove the 1st occurrence of 10
 
+
 basket.pop(4)  # Remove the 4th index element (5)
+print(basket.index(8))  # Get the index of 8
+print (2 in basket)
+print(basket.count(2))
+# Cannot print out basket.sort() because it returns None
+# Sorting a list - SORT
+basket.sort() # Create a new list with the sorted values
 print(basket) 
+basket.reverse() # Create a new list with the sorted values
+print(basket)  # Sort the list in ascending order
