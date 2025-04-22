606904d8550d5df2b46dfd20f3f5727b2986e4a7
Anh Hoang <anh.comdr@gmail.com>
2025-04-22 13:02:45 +0300

    Create Lession4_Dictionary.py

---
+++ Fundamental 2/Lession4_Dictionary.py
@@ -0,0 +1,7 @@
+# Dictionary: A dictionary is a collection of key-value pairs. Each key is unique and maps to a value.
+dict = {
+    "name": "John",
+    "age": 30,
+}
+print(dict)  # Output: {'name': 'John', 'age': 30}
+print(dict["name"])  # Output: John
