3d61223df53e3cb06df7cfd409b33251bb4908bf
Anh Hoang <anh.comdr@gmail.com>
2025-04-23 22:22:05 +0300

    Operator

---
+++ Fundamental 3/2_logical_operator.py
@@ -0,0 +1,12 @@
+is_Friend = False
+is_User = True
+
+if is_Friend or is_User:
+    print("You are a friend or a user.")
+if is_Friend and is_User:
+    print("You are a friend and a user.")
+
+# <> == != >= <= >= 
+# not(is_Friend or is_User) == False
+# and or
+# not True == False
