e5f8ab48ea75b09fff5156a4c9791664017bf502
Anh Hoang <anh.comdr@gmail.com>
2025-04-24 06:50:22 +0300

    For loop

---
+++ Fundamental 3/4_For_Loop.py
@@ -0,0 +1,21 @@
+# For loop in string
+for item in ' Hoang cong anh':
+    print(item)
+# For loop in a list
+for item in ['Hoang', 'cong', 'anh']:
+    print(item)
+# For loop in a tuple
+for item in ('Hoang', 'cong', 'anh'):
+    print(item)
+
+# For loop in a set
+for item in {'Hoang', 'cong', 'anh'}:
+    print(item)
+
+# For loop in a dictionary
+dictionary_0 = {
+    "name": "John",
+    "age": 30,
+}
+for value in dictionary_0.values():
+    print(value)  # Output: John 30
