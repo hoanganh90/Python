ec0d0840b13fcc3786f57e0b499f2ebc484753ad
Anh Hoang <anh.comdr@gmail.com>
2025-04-26 06:55:01 +0300

    For in dictionary

--- Fundamental 3/4_For_Loop.py
+++ Fundamental 3/4_For_Loop.py
@@ -16,9 +16,22 @@ for item in {'Hoang', 'cong', 'anh'}:
 dictionary_0 = {
     "name": "John",
     "age": 30,
+    "city": "New York",
+    "country": "USA"
+
 }
 for value in dictionary_0.values():
     print(value)  # Output: John 30
     
 for key in dictionary_0:
     print(dictionary_0[key]) 
+
+for item in dictionary_0.items():
+    print(item)  
+for key in dictionary_0.keys():
+    print(key) 
+for item in dictionary_0.items():
+    key, value = item
+    print(key, value )  
+for key, value in dictionary_0.items():
+    print(key, value )  
