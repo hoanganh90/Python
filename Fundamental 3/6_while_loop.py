5188b50a90fae8092358f0399bbe0975ba8e2d1b
Anh Hoang <anh.comdr@gmail.com>
2025-04-29 15:45:22 +0300

    While loop

--- Fundamental 3/6_while_loop.py
+++ Fundamental 3/6_while_loop.py
@@ -7,3 +7,24 @@ while i < 5:
     break
 else:
     print("Loop ended") # The else block is executed when the while loop condition becomes false.
+
+# While True
+while True:
+    input(" Say something: ")
+    break # The break statement is used to exit the loop.
+
+while True:
+    response = input("Say something: ")
+    if(response == "exit"):
+        break
+    print("You said: " + response) 
+
+#pass
+my_list = [1, 2, 3, 4, 5]
+for item in my_list:
+    pass
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+    pass # The pass statement is a null operation; it is syntactically required but does nothing.
