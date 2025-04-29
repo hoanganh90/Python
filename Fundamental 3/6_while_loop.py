a8847c58fcf76a00420fe814d02d2c00a4e0d661
Anh Hoang <anh.comdr@gmail.com>
2025-04-29 15:34:48 +0300

    While loop

--- Fundamental 3/6_while_loop.py
+++ Fundamental 3/6_while_loop.py
@@ -2,5 +2,8 @@
 # While loop is used to execute a block of code repeatedly as long as a given condition is true.
 i = 0
 while i < 5:
-    print(i)
     i += 1
+    print(i)
+    break
+else:
+    print("Loop ended") # The else block is executed when the while loop condition becomes false.
