f0b14ea90a8f10a75c3b27dec9d540b35658d569
Anh Hoang <anh.comdr@gmail.com>
2025-05-01 21:15:44 +0300

    Methods and function

---
+++ Fundamental 4/4_MethodsVsFunctions.py
@@ -0,0 +1,7 @@
+# Method in Python is a function that is associated with an object. 
+# Using the dot operator, we can call a method on an object.
+
+
+
+# Functions are defined using the def keyword, 
+# while methods are defined within a class and are called on instances of that class.
