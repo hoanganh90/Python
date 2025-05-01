0d8bb9d50cc6546e2e9756775a8e9db2a90d3a8f
Anh Hoang <anh.comdr@gmail.com>
2025-05-01 21:19:07 +0300

    Docstring

--- Fundamental 4/4_MethodsVsFunctions.py
+++ Fundamental 4/4_MethodsVsFunctions.py
@@ -5,3 +5,15 @@
 
 # Functions are defined using the def keyword, 
 # while methods are defined within a class and are called on instances of that class.
+
+
+# Docstrings are a way to document your code and provide information about what a function or method does.
+# They are enclosed in triple quotes and can be accessed using the __doc__ attribute of the function or method.
+def test(a):
+    '''
+    This is a test function that takes an argument a and returns its square.
+    '''
+    print(a**2)
+    return a**2
+print(test.__doc__) # This will print the docstring of the test function.
+print(test(5)) # This will print the square of 5, which is 25
