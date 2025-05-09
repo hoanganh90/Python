b4ed72f2d74e4a037c33c49ffa390df004ded753
Anh Hoang <anh.comdr@gmail.com>
2025-05-09 22:25:25 +0300

    Inheritance 1

--- OOP/7_Inheritance.py
+++ OOP/7_Inheritance.py
@@ -2,6 +2,21 @@ class User:
     def sign_in(self):
         print("User signed in")
 class Wizard(User):
-    pass
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+    def attack(self):
+        print(f"Attacking with power {self.power}")
 class Archer(User):
-    pass
+    def __init__(self,name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+    def attack(self):
+        print(f"Attacking with arrows: {self.num_arrows} left")
+# Example usage
+wizard1 = Wizard( "Merlin", 50)
+print(wizard1.sign_in()) # User signed in
+wizard1.attack() # Attacking with power 50
+archer1 = Archer("Robin", 100)
+print(archer1.sign_in()) # User signed in
+archer1.attack() 
