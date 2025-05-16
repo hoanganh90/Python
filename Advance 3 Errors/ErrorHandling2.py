f8c33eb31726086d30fb19d23d22f93d9fc47ab7
Anh Hoang <anh.comdr@gmail.com>
2025-05-16 07:01:16 +0300

    Error handling

---
+++ Advance 3 Errors/ErrorHandling2.py
@@ -0,0 +1,27 @@
+# Error Handling
+# Error handling is a way to respond to errors that occur in your program.
+# It allows you to gracefully handle errors and continue executing your program.
+# In Python, you can use try and except blocks to handle errors.
+def hello():
+    try:
+        # This will raise a NameError because 'name' is not defined
+        print(f"Hello, {name}!")
+    except NameError as e:
+        # This block will execute if a NameError occurs
+        print(f"An error occurred: {e}")
+    finally:
+        # This block will always
+        print("This will always execute")
+
+hello()
+
+age = input('Whaat is your age?: ')
+try: 
+    age = int(age)
+    print(f'Your age is {age}')
+except ValueError as age_error:
+    print('Please enter a valid number ' + str(age_error))
+else:
+    print('No errors occurred')
+finally:
+    print('Thank you!')
