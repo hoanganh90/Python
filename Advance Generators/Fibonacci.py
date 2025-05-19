3399d8f71fc83b6368f6a1ae960cb6c0a39b2ea7
Anh Hoang <anh.comdr@gmail.com>
2025-05-19 14:04:48 +0300

    Add files via upload

--- Advance Generators/Fibonacci.py
+++ Advance Generators/Fibonacci.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
+def fibonacci(n): # This is a generator function. It is more memory efficient than the second one.
     a=0
     b=1
     for i in range(n):
@@ -6,5 +6,17 @@ def fibonacci(n):
         temp = a
         a = b
         b = temp + b
+
+def fibonacci_2(n): # This way is not recommended because it uses more memory
+    a = 0
+    b = 1
+    result = []
+    for i in range(n):
+        result.append(a)
+        temp = a
+        a = b
+        b = temp + b
+    return result
+
 for x in fibonacci(10):
     print(x)
