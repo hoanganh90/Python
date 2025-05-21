71e7adfbe682ff25fc29a6362e714998cd049771
Anh Hoang <anh.comdr@gmail.com>
2025-05-21 22:03:13 +0300

    Useful module

---
+++ Advance Generators/usefulModule.py
@@ -0,0 +1,11 @@
+from collections import Counter
+from datetime import datetime
+print(datetime.now()) # datetime is used to get the current date and time
+print(datetime.now().date()) # datetime is used to get the current date
+print(datetime.now().time()) # datetime is used to get the current time
+li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(Counter(li)) # Counter is used to count the number of occurrences of each element in the list
+senetence = "I love python. Python is a great programming language. Python is easy to learn."
+print(Counter(senetence)) # Counter is used to count the number of occurrences of each character in the string
+normal_dict = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
+
