93fed94863916e7a019392b91f08843ef7ac2105
Anh Hoang <anh.comdr@gmail.com>
2025-05-22 07:41:23 +0300

    File

--- File IO/script.py
+++ File IO/script.py
@@ -5,3 +5,12 @@ my_file.close()
 my_file = open('File IO/test.txt', "r")
 print(my_file.read())
 my_file.close
+
+with open('File IO/test.txt', "r") as my_file:
+    print(my_file.read())
+with open('File IO/test.txt', "w") as my_file_w:
+    my_file_w.write("Hello World 2")
+with open('File IO/test.txt', "r+") as my_file_rw:
+    my_file_rw.write("Hello World 3")
+with open('File IO/test.txt', "r") as my_file:
+    print(my_file.read())   
