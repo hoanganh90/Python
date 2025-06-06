aa98c1725b29df77b5609e5df2f44f71ae604521
Anh Hoang <anh.comdr@gmail.com>
2025-06-06 14:56:06 +0300

    Add watermark

--- Scripting/PDFs/waterMarker.py
+++ Scripting/PDFs/waterMarker.py
@@ -1,5 +1,14 @@
 import PyPDF2
 
-template = PyPDF2.PdfFileReader(open('dummy.pdf','rb'))
+template = PyPDF2.PdfFileReader(open('combined.pdf','rb')) # Loop through all pages of the pdf file
 watermark = PyPDF2.PdfFileReader(open('wtr.pdf','rb'))
 output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('combinePdf_output.pdf','wb') as file:
+        output.write(file)
+
