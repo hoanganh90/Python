8d74f061e9832ad4695daf6c5ea652e3ce207f3d
Anh Hoang <anh.comdr@gmail.com>
2025-12-20 06:42:59 +0200

    Update code

--- Fundamental 2/Lession4_Dictionary.py
+++ Fundamental 2/Lession4_Dictionary.py
@@ -32,4 +32,44 @@ print(user3) # Output: {'name': 'John', 'age': 30}
 user3.update({'name': 'Jane'})
 print(user3.get('name')) # Output: Jane
 
+my_dict = {'name': 'Alice', 'age': 35, 'city': 'New York'}
+my_dict['country'] = 'USA' # This will add the key-value pair to the dictionary.
+print(my_dict) # Output: {'name': 'Alice', 'age': 35, 'city': 'New York', 'country': 'USA'}
+# Delete an item from a dict
+del my_dict['name']
+print(my_dict)
+# POP an item from a dict
+my_dict.pop("age")
+print(my_dict)
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+my_dict.popitem()
+print(my_dict)
 
+#Exercise 9: Modify Nested Dictionary
+nested_student_dict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+print(f"Nested dictionary: {nested_student_dict}")
+# Change the value name by call the key
+nested_student_dict['class']['student']['name'] = "Jessica"
+# Print new dict
+print(f"New nested dict: {nested_student_dict}")
+
+# Exercise 11: Create a dictionary by extracting the keys from a give dict
+sample_dict = {
+    "name": "Kelly",
+    "age":25,
+    "salary": 8000,
+    "city": "New York"
+}
+keys = ["name", "salary"]
+new_dict = { k: sample_dict[k] for k in keys}
+print("New dict is: " , new_dict)
