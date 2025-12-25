ccf492af1c43d3b289d2e3581c7662db03109633
Anh Hoang <anh.comdr@gmail.com>
2025-12-25 15:59:41 +0200

    Move file to a new folder

---
+++ Practice/WarmUp/13.birthDayCakeCandles.py
@@ -0,0 +1,4 @@
+def birthdayCakeCandles(candles):
+    tallestCandle = max(candles)
+    print(candles.count(tallestCandle))
+birthdayCakeCandles([4,4,1,3])
---
+++ Practice/WarmUp/14.timeConversion.py
@@ -0,0 +1,21 @@
+def timeConversion(s):
+    # Write your code here
+    hour = int(s[:2])
+    timeInfo = s[-2:]
+    remaingInfo = s[2:-2]
+    print(hour)
+    print(timeInfo)
+    time24h = ""
+    if timeInfo == "PM":
+        if hour < 12:
+            time24h = str(hour + 12) + remaingInfo
+        else: time24h = str(hour) + remaingInfo
+    else:
+        if hour == 12:
+            time24h = "00" + remaingInfo
+        else: time24h = s[: -2]
+
+    print(time24h)
+
+
+timeConversion("02:01:00PM")
